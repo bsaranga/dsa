bbeed210f988ab09a966dff1f28a5dd346f4f911
Buwaneka Saranga <bsaranga@gmail.com>
2024-05-01 12:39:28 +0530

    Refactor CircularBuffer class to include a remove method

--- data_structures/circular_buffer.py
+++ data_structures/circular_buffer.py
@@ -1,9 +1,17 @@
 class CircularBuffer():
-    def __init__(self, size, pointer=0):
+    def __init__(self, size, start=0):
+        self.start = start
         self.size = size
         self.buffer = [None] * size
-        self.pointer = pointer
+        self.pointer = start
+
+    def __str__(self) -> str:
+        return f"Buffer: {self.buffer}"
 
     def append(self, value):
         self.buffer[self.pointer] = value
         self.pointer = (self.pointer + 1) % self.size
+
+    def remove(self):
+        self.buffer[self.start] = None
+        self.start = (self.start + 1) % self.size
